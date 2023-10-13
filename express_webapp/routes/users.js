c2f3f9e6f2c0cd60205eab0d7eb414b1ba13a175
noepierre <noepi2004@gmail.com>
2023-10-13 15:33:14 +0200

    activities vue

--- express_webapp/routes/users.js
+++ express_webapp/routes/users.js
@@ -11,37 +11,54 @@ router.get('/', async function(req, res, next) {
     socker = req.session;
 
     try {
-    res.render('users');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Erreur pour ajouter un utilisateur');
-  }
+        res.render('users');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erreur pour ajouter un utilisateur');
+    }
 });
 
 router.post('/', async function(req, res, next) {
-  const userData = req.body;
-  try {
-      // Vérification que l'adresse e-mail de l'utilisateur est unique
-      const existingUser = await user_dao.findByEmail(userData.email);
-      if (existingUser) {
-          return res.status(400).send('L\'adresse e-mail est déjà utilisée.');
-      }
+    const userData = req.body;
+    socket = req.session;
+    try {
+        // Vérification que l'adresse e-mail de l'utilisateur est unique
+        const existingUser = await user_dao.findByEmail(userData.email);
+        if (existingUser) {
+            return res.status(400).send('L\'adresse e-mail est déjà utilisée.');
+        }
 
-      // Création de l'utilisateur en utilisant la classe UserDAO
-      try {
-          await user_dao.insert(userData);
-          // Sauvegardez ici l'ID de l'utilisateur dans la session
-          socker.userId = userData.id;
+        // Création de l'utilisateur en utilisant la classe UserDAO
+        try {
+            await user_dao.insert(userData);
+            // res.status(201).send(`Utilisateur créé avec l'ID avec succès`);
+            try {
+                // Vérification des informations de connexion
+                const user = await user_dao.findByEmail(userData.email);
+                if (!user) {
+                    return res.status(400).send("L'adresse e-mail est incorrecte.");
+                }
 
-          res.redirect('/');
-      } catch (error) {
-          console.error(error);
-          res.status(500).send('Erreur lors de l\'ajout de l\'utilisateur');
-      }
+                const userId = await user_dao.connectUserByEmail(user.email, user.password);
+                if (!userId) {
+                    return res.status(400).send('Le mot de passe est incorrect.');
+                } else {
+                    // Stockage de l'ID utilisateur dans la session
+                    socket.userId = userId;
+                    res.redirect('/');
+                }
+            } catch (error) {
+                console.error(error);
+                res.status(500).send('Erreur lors de la tentative de connexion');
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Erreur lors de l\'ajout de l\'utilisateur');
+        }
     } catch (error) {
-      console.error(error);
-      res.status(500).send('Erreur lors de l\'ajout de l\'utilisateur');
-  }
+        console.error(error);
+        res.status(500).send('Erreur lors de l\'ajout de l\'utilisateur');
+    }
 });
 
 module.exports = router;
