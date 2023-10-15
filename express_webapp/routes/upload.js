2d59d4970d30eaebcf24d25d378a95851ff57938
Oscar <pavoine.e2202962@etud.univ-ubs.fr>
2023-10-15 16:13:27 +0200

    Calculs stable/fini

--- express_webapp/routes/upload.js
+++ express_webapp/routes/upload.js
@@ -4,13 +4,11 @@ var router = express.Router();
 const fileUpload = require('express-fileupload');
 var activity_dao = require('../../sport-track-db/sport-track-db').activity_dao;
 
-
 // Configuration du middleware express-session
-router.use(session({secret: 'votre-secret-secret',resave: false, saveUninitialized: true, }));
+router.use(session({ secret: 'votre-secret-secret', resave: false, saveUninitialized: true }));
 router.use(fileUpload());
 
-
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   try {
     res.render('upload');
   } catch (error) {
@@ -46,15 +44,17 @@ router.post('/', async (req, res) => {
       minHeartRate: 0,
     };
 
-    activity_dao.insert(dataActivity);
+    await activity_dao.insert(dataActivity);
     const lastActivityId = await activity_dao.getLastActivityId();
+
+
     for (const data of infoData) {
       data.date = infoActivityDate;
       data.description = infoActivityDescription;
-      activity_dao.insertFile(lastActivityId, data);
+      await activity_dao.insertFile(lastActivityId, data);
     }
 
-    calculateAndUpdateActivityValues(lastActivityId);
+    await calculateAndUpdateActivityValues(lastActivityId);
 
     console.log('Upload terminé');
     res.redirect('/activities');
@@ -63,7 +63,6 @@ router.post('/', async (req, res) => {
     res.status(500).send('Erreur lors de l`upload');
   }
 });
-
 
 async function calculateAndUpdateActivityValues(activityId) {
   try {
@@ -83,18 +82,13 @@ async function calculateAndUpdateActivityValues(activityId) {
     let previousAltitude = null;
     let i = 0;
 
-    const points = []; // Liste pour stocker les données de latitude et longitude
+    const points = [];
 
     for (const data of datas) {
-      if (startTime === '00:00:00') {
-        startTime = data.time;
-        endTime = data.time;
-      }
-      if (data.time > endTime) {
-        endTime = data.time;
-      } else if (data.time < startTime) {
+      if (i === 0) {
         startTime = data.time;
       }
+      endTime = data.time;
       if (data.speed > maxSpeed) {
         maxSpeed = data.speed;
       }
@@ -112,29 +106,18 @@ async function calculateAndUpdateActivityValues(activityId) {
       }
       previousAltitude = data.altitude;
 
-      // Ajoutez les données de latitude et longitude à la liste des points
       points.push({ latitude: data.latitude, longitude: data.longitude });
-
       i += 1;
     }
 
     averageHeartRate = averageHeartRate / i;
-
-    startTime = convertToTimeObject(startTime);
-    endTime = convertToTimeObject(endTime);
-
-    const differenceInMillis = endTime.getTime() - startTime.getTime();
-    const differenceInHours = Math.floor(differenceInMillis / (1000 * 60 * 60));
-    const differenceInMinutes = Math.floor((differenceInMillis % (1000 * 60 * 60)) / (1000 * 60));
-    const differenceInSeconds = Math.floor((differenceInMillis % (1000 * 60)) / 1000);
-
-    time = `${formatTimePart(differenceInHours)}:${formatTimePart(differenceInMinutes)}:${formatTimePart(differenceInSeconds)}`;
-
-    // Calcul de la distance totale à partir des données de latitude et longitude
     distance = calculDistanceTrajet({ points });
-
+    const startTimeDate = convertToTimeObject(startTime);
+    const endTimeDate = convertToTimeObject(endTime);
+    const differenceInMillis = endTimeDate - startTimeDate;
     averageSpeed = distance / (differenceInMillis / (1000 * 60 * 60));
 
+    time = formatTimePart(Math.floor(differenceInMillis / (1000 * 60 * 60))) + ':' + formatTimePart(Math.floor((differenceInMillis / (1000 * 60)) % 60)) + ':' + formatTimePart(Math.floor((differenceInMillis / 1000) % 60));
     averageSpeed = averageSpeed.toFixed(2);
     distance = distance.toFixed(3);
     averageHeartRate = averageHeartRate.toFixed(2);
@@ -150,7 +133,7 @@ async function calculateAndUpdateActivityValues(activityId) {
       minHeartRate: minHeartRate,
     };
 
-    activity_dao.update(activityId, ret);
+    await activity_dao.update(activityId, ret);
   } catch (error) {
     console.error(error);
   }
@@ -159,25 +142,24 @@ async function calculateAndUpdateActivityValues(activityId) {
 function degToRad(degrees) {
   return degrees * (Math.PI / 180);
 }
-function calculDistance2PointsGPS(lat1, lon1, lat2, lon2) {
-  const R = 6378.137; // Rayon de la Terre en kilomètres
 
-  // Conversion des latitudes et longitudes en radians
+function calculDistance2PointsGPS(lat1, lon1, lat2, lon2) {
+  const R = 6378.137;
+
   const lat1Rad = degToRad(lat1);
   const lon1Rad = degToRad(lon1);
   const lat2Rad = degToRad(lat2);
   const lon2Rad = degToRad(lon2);
 
-  // Calcul de la distance en utilisant la formule haversine
   const d = R * Math.acos(Math.sin(lat2Rad) * Math.sin(lat1Rad) + Math.cos(lat2Rad) * Math.cos(lat1Rad) * Math.cos(lon2Rad - lon1Rad));
 
   return d;
 }
+
 function calculDistanceTrajet(activite) {
-  const points = activite.points; // Supposons que "points" est un tableau d'objets {latitude, longitude}
+  const points = activite.points;
   let distanceTotale = 0;
 
-  // Calcul de la distance entre chaque paire de points consécutifs
   for (let i = 1; i < points.length; i++) {
     const point1 = points[i - 1];
     const point2 = points[i];
@@ -187,6 +169,7 @@ function calculDistanceTrajet(activite) {
 
   return distanceTotale;
 }
+
 function convertToTimeObject(timeStr) {
   const [hours, minutes, seconds] = timeStr.split(':');
   const date = new Date();
@@ -195,6 +178,7 @@ function convertToTimeObject(timeStr) {
   date.setSeconds(parseInt(seconds, 10));
   return date;
 }
+
 function formatTimePart(part) {
   return part.toString().padStart(2, '0');
 }
